6df5029ca3cfab09f13b617820f902669017e086
Pavel Shukhman <pavel@reliza.io>
2025-05-23 11:21:53 -0400

    chore: cump ReARM landing to react 19

--- landing_site/src/index.js
+++ landing_site/src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop >
         <App />
       </ScrollToTop>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
