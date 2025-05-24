28642fdd6d2955cffb5429d63d62ea1f103b307e
Pavel Shukhman <pavel@reliza.io>
2025-05-24 16:56:10 -0400

    chore: cleanup Blog home

--- landing_site/src/Pages/Blog/Blog.js
+++ landing_site/src/Pages/Blog/Blog.js
@@ -1,35 +1,13 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import { useEffect, useState } from 'react'
-import grayMatter from 'gray-matter'
-import rearmHistoryPost from '../../posts/rearm-history.md'
 import BasicLayout from '../../Layout/BasicLayout/BasicLayout'
 import styles from "./Blog.module.css"
-import about1 from "../../Assets/AboutUs/about1.png"
-import about2_1 from "../../Assets/AboutUs/about2_1.png"
-import about2_2 from "../../Assets/AboutUs/about2_2.png"
 import LastContainer1 from '../../Components/LastContainer/LastContainer1'
-import Experience from '../../Components/Experience/Experience'
 import { Buffer } from 'buffer'
 import posts from "../../utils/LoadPosts"
 
 window.Buffer = Buffer
 
-// const Post = ({ postName }) => {
-//   const [content, setContent] = useState('');
-//   const [meta, setMeta] = useState({});
-
-//   useEffect(() => {
-//       const filePath = path.join('src/posts', `${postName}.md`);
-//       const fileContent = fs.readFileSync(filePath, 'utf8');
-//       const { data, content } = grayMatter(fileContent);
-//       setMeta(data);
-//       setContent(content);
-//   }, [postName]);
-
 const Blog = () => {
-  const { data, content } = grayMatter(rearmHistoryPost)
-
   return (
     <BasicLayout>
       <div className={`${styles.containerHeader} container-fluid`}>
