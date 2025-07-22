45ef265b4b28de122e20684be4e498af7a1ea32d
Pavel Shukhman <pavel@reliza.io>
2025-07-22 13:45:58 -0400

    fix(ui): extend statistics counts and modify view to use data table

--- ui/src/components/AnalyticsOfOrg.vue
+++ ui/src/components/AnalyticsOfOrg.vue
@@ -1,220 +1,95 @@
 <template>
-    <div>
-        <h4>Statistics</h4>
-        <!-- Container for the visualization -->
-        <div class="charts">
-            <div id="releaseCreationVis"></div>
-            <div id="instanceChangesVis"></div>
-            <div id="leadTimeVis"></div>
-            <div id="releasePercentVis"></div>
-        </div>
-        <div class="numericAnalyticsBlock" v-if="Object.entries(totalsAnalytics).length">
-            <div class="numericAnalytics">
-                <div>Components: {{ totalsAnalytics.components }}</div>
-                <div>Products: {{ totalsAnalytics.products }}</div>
-                <div>Branches and Feature Sets: {{ totalsAnalytics.branches }}</div>
-                <div>Releases: {{ totalsAnalytics.releases }}</div>
-                <!-- div>Instances: {{ totalsAnalytics.instances }}</div -->
-                <div>VCS Repositories: {{ totalsAnalytics.vcs }}</div>
-                <div>Recorded Commits: {{ totalsAnalytics.commits }}</div>
-                <div>Recorded Artifacts: {{ totalsAnalytics.artifacts }}</div>
-            </div>
+    <div class="componentView">
+        <h4>Statistics for {{ myorg.name }}</h4>
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+            <n-data-table
+                :columns="columns"
+                :data="analyticsData"
+                :bordered="false"
+            />
         </div>
     </div>
 </template>
-  
-<script lang="ts" setup>
-import * as vegaEmbed from 'vega-embed'
-import axios from '../utils/axios'
-import { ComputedRef, h, ref, Ref, computed, onMounted } from 'vue'
-import { useStore } from 'vuex'
-import gql from 'graphql-tag'
-import graphqlClient from '../utils/graphql'
 
-const store = useStore()
+<script lang="ts">
+import { defineComponent, ref, onMounted, computed, Ref, ComputedRef } from 'vue';
+import { useStore } from 'vuex';
+import gql from 'graphql-tag';
+import graphqlClient from '../utils/graphql';
+import { NDataTable } from 'naive-ui';
 
+export default defineComponent({
+    name: 'AnalyticsOfOrg',
+    components: {
+        NDataTable
+    },
+    setup() {
+        const store = useStore();
+        const isLoading = ref(true);
+        const totalsAnalytics = ref({});
 
-const instVisData : Ref<any> = ref({ 
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    background: '#f7f7f7',
-    title: 'Number of Instance Changes Per Day',
-    width: 'container',
-    data: {
-        values: []
-    },
-    mark: {
-        type: 'point',
-        tooltip: true
-    },
-    encoding: {
-        y: {
-            field: 'num',
-            type: 'quantitative',
-            aggregate: 'sum',
-            axis: {
-                title: '# of changes'
+        const myorg: ComputedRef<any> = computed(() => store.getters.myorg);
+
+        const analyticsData = computed(() => {
+            if (!totalsAnalytics.value || Object.keys(totalsAnalytics.value).length === 0) {
+                return [];
             }
-        },
-        x: {
-            field: 'date',
-            type: 'temporal',
-            timeUnit: 'utcyearmonthdate',
-            axis: {
-                title: 'Date'
-            }
-        }
-    }
-})
-const leadTimeVisData: Ref<any> = ref({
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    background: '#f7f7f7',
-    title: 'Lead Time Per Environment',
-    width: 'container',
-    data: {
-        values: []
-    },
-    mark: {
-        type: 'line',
-        tooltip: true
-    },
-    transform: [
-        { filter: 'datum.duration > 0' },
-        { calculate: 'datum.duration/3600', as: 'duration_hours' }
-    ],
-    encoding: {
-        y: {
-            field: 'duration_hours',
-            type: 'quantitative',
-            aggregate: 'average',
-            axis: {
-                title: 'Lead Time, hours'
-            }
-        },
-        x: {
-            field: 'dateFrom',
-            type: 'temporal',
-            timeUnit: {
-                unit: 'yearmonthdate',
-                step: 7
+            return Object.entries(totalsAnalytics.value)
+                .map(([key, value]) => ({
+                    metric: key === 'vcs' ? 'VCS Repositories' : key.charAt(0).toUpperCase() + key.slice(1).replace(/_/g, ' '),
+                    value
+                }))
+                .sort((a, b) => a.metric.localeCompare(b.metric));
+        });
+
+        const columns = [
+            {
+                title: 'Metric',
+                key: 'metric'
             },
-            axis: {
-                title: 'Date'
+            {
+                title: 'Count',
+                key: 'value'
             }
-        },
-        color: {
-            field: 'environment',
-            type: 'nominal'
-        }
-    }
-})
-const releasePercentVisData: Ref<any> = ref({
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    background: '#f7f7f7',
-    title: 'Percent Releases Deployed Per Environment',
-    width: 'container',
-    data: {
-        values: []
-    },
-    layer: [
-        { mark: 'bar' },
-        {
-            mark: {
-                type: 'text',
-                align: 'left',
-                baseline: 'middle',
-                dx: 3
-            },
-            encoding: {
-                text: { field: 'percentage', type: 'quantitative' }
-            }
-        }
-    ],
-    encoding: {
-        y: {
-            field: 'environment',
-            type: 'nominal',
-            axis: {
-                title: 'Environments'
-            }
-        },
-        x: {
-            field: 'percentage',
-            type: 'quantitative',
-            axis: {
-                title: 'Percent of Releases'
-            },
-            scale: { domain: [0, 100] }
-        }
-    }
-})
-const totalsAnalytics: Ref<any> =  ref({})
-       
-const props = defineProps<{
-    orguuid?: string
-}>()
-const isLoading: Ref<boolean> = ref(true)
-function fetchInstanceAnalytics(orgUuid: string) {
-    axios.get('/v1/home/instanceAnalytics/' + orgUuid).then(response => {
-        response.data.map((item: any) => {
-            item.date = item.date.split('[')[0]
-        })
-        instVisData.value.data.values = response.data
-        vegaEmbed.default('#instanceChangesVis', instVisData.value).then(() => {
-            isLoading.value = false
-        })
-    })
-}
-function fetchOrgReleasesLeadTimes(orgUuid: string) {
-    axios.get('/api/manual/v1/release/listSoftLeadTimeByOrg/' + orgUuid).then(response => {
-        leadTimeVisData.value.data.values = response.data
-        vegaEmbed.default('#leadTimeVis', leadTimeVisData.value)
-    })
-}
-function fetchReleaseEnvPercentages(orgUuid: string) {
-    axios.get('/api/manual/v1/release/percentagesByOrg/' + orgUuid).then(response => {
-        releasePercentVisData.value.data.values = Object.keys(response.data).map((k, i) => {
-            return { 'environment': k, 'percentage': parseFloat(response.data[k]).toFixed(2) }
-        })
-        vegaEmbed.default('#releasePercentVis', releasePercentVisData.value)
-    })
-}
-async function fetchTotalsAnalytics(orgUuid: string) {
-    const resp = await graphqlClient.query({
-        query: gql`
-            query totalsAnalytics($orgUuid: ID!) {
-                totalsAnalytics(orgUuid: $orgUuid)
-            }
-            `,
-        variables: { orgUuid },
-        fetchPolicy: 'no-cache'
-    })
-    totalsAnalytics.value = resp.data.totalsAnalytics
-}
-const myorg: ComputedRef<any> = computed((): any => store.getters.myorg)
-const myuser = store.getters.myuser
+        ];
 
-onMounted(() => {
-    if (false && myuser.installationType !== 'OSS') fetchInstanceAnalytics(myorg.value.uuid)
-    if (false && myuser.installationType !== 'OSS') fetchOrgReleasesLeadTimes(myorg.value.uuid)
-    if (false && myuser.installationType !== 'OSS') fetchReleaseEnvPercentages(myorg.value.uuid)
-    fetchTotalsAnalytics(myorg.value.uuid)
-    if (false && myuser.installationType !== 'OSS') store.dispatch('fetchProperties', myorg.value.uuid)
-})
+        async function fetchTotalsAnalytics(orgUuid: string) {
+            try {
+                const resp = await graphqlClient.query({
+                    query: gql`
+                        query totalsAnalytics($orgUuid: ID!) {
+                            totalsAnalytics(orgUuid: $orgUuid)
+                        }
+                    `,
+                    variables: { orgUuid },
+                    fetchPolicy: 'no-cache'
+                });
+                totalsAnalytics.value = resp.data.totalsAnalytics;
+            } catch (error) {
+                console.error('Error fetching totals analytics:', error);
+            }
+        }
+
+        onMounted(async () => {
+            if (myorg.value && myorg.value.uuid) {
+                await fetchTotalsAnalytics(myorg.value.uuid);
+            }
+            isLoading.value = false;
+        });
+
+        return {
+            isLoading,
+            analyticsData,
+            columns,
+            myorg
+        };
+    }
+});
 </script>
-  
-<style scoped lang="scss">
-.vega-actions a {
-    margin-right: 5px;
-}
 
-.charts {
-    display: grid;
-    grid-template-columns: 0.95fr 0.95fr;
+<style scoped>
+.componentView {
+    padding: 20px;
 }
-
-.numericAnalyticsBlock {
-    margin-left: 20px;
-}
-
 </style>
-  
