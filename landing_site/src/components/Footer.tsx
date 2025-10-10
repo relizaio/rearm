fc7dc51c13d9d5ba8e7d0ae9063ff682b399405e
Pavel Shukhman <pavel@reliza.io>
2025-10-10 18:57:11 -0400

    fix(landing): add x link

--- landing_site/src/components/Footer.tsx
+++ landing_site/src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { FaDiscord, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { FaBluesky } from 'react-icons/fa6';
+import { FaXTwitter } from 'react-icons/fa6';
 
 export default function Footer() {
   return (
@@ -15,6 +16,9 @@ export default function Footer() {
           <div className="footer_social">
             <a className="footer_social_link" href="https://discord.gg/UTxjBf9juQ" target="_blank" rel="noopener noreferrer" title="Discord">
               <FaDiscord />
+            </a>
+            <a className="footer_social_link" href="https://twitter.com/relizaio" target="_blank" title="Twitter">
+              <FaXTwitter />
             </a>
             <a className="footer_social_link" href="https://bsky.app/profile/reliza.bsky.social" target="_blank" rel="noopener noreferrer" title="BlueSky">
               <FaBluesky />
