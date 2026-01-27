c19895a27aa4dda8125e7184ed81a01db4ea35e3
Pavel Shukhman <pavel@reliza.io>
2026-01-27 17:21:27 -0500

    chore(backend): memory settings for backend runtime

--- backend/start-core.sh
+++ backend/start-core.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app
-java -Djava.security.egd=file:/dev/./urandom -Dlog4j2.formatMsgNoLookups=true -XX:+ShowCodeDetailsInExceptionMessages org.springframework.boot.loader.launch.JarLauncher
+java -Djava.security.egd=file:/dev/./urandom -Dlog4j2.formatMsgNoLookups=true -XX:+ShowCodeDetailsInExceptionMessages org.springframework.boot.loader.launch.JarLauncher -Xms1g -Xmx3g
