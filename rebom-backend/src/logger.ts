df8136950edc1840c3c22803803f9786a9c8c114
Pavel Shukhman <pavel@reliza.io>
2026-03-17 13:30:58 -0400

    fix(rebom): support jsonized logs

--- rebom-backend/src/logger.ts
+++ rebom-backend/src/logger.ts
@@ -2,16 +2,27 @@ import pino from 'pino';
 import pinoPretty from 'pino-pretty';
 
 const logLevel = process.env.LOG_LEVEL || process.env.NODE_ENV === 'development' ? 'debug' : 'info';
+const logType = process.env.LOG_TYPE || 'plain';
 
-export let logger = pino(
-  {
-    level: logLevel
-  },
-  pinoPretty({
-    colorize: true,
-    translateTime: 'yyyy-mm-dd HH:MM:ss.l',
-    ignore: 'pid,hostname'
-  })
-);
+export let logger = logType === 'json'
+  ? pino({
+      level: logLevel,
+      formatters: {
+        level: (label) => {
+          return { level: label };
+        }
+      },
+      timestamp: pino.stdTimeFunctions.isoTime
+    })
+  : pino(
+      {
+        level: logLevel
+      },
+      pinoPretty({
+        colorize: true,
+        translateTime: 'yyyy-mm-dd HH:MM:ss.l',
+        ignore: 'pid,hostname'
+      })
+    );
 
 export type Logger = typeof logger;
